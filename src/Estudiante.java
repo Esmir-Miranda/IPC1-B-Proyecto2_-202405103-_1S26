8707b1a3cb4bf1cdc39b315de677849765113df3
Esmir Miranda <esmirmiranda2007@gmail.com>
2026-04-23 21:46:16 -0600

    Agregar student

---
+++ src/Estudiante.java
@@ -0,0 +1,23 @@
+package modelo;
+
+public class Estudiante extends Usuario {
+    private int cursosInscritos;
+
+    public Estudiante(String codigo, String nombre, String fechaNacimiento, String genero, String password) {
+        super(codigo, nombre, fechaNacimiento, genero, password);
+        this.cursosInscritos = 0;
+    }
+
+    public int getCursosInscritos() {
+        return cursosInscritos;
+    }
+
+    public void setCursosInscritos(int cursosInscritos) {
+        this.cursosInscritos = cursosInscritos;
+    }
+
+    @Override
+    public String getRol() {
+        return "ESTUDIANTE";
+    }
+}
